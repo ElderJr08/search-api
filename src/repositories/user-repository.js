2da5033cdabb9b19e5f694c96c804d8021cb96ae
Elder Junior <elderjunior08@gmail.com>
2021-06-28 01:27:57 -0300

    feat: Implementa camada de repository para users

---
+++ src/repositories/user-repository.js
@@ -0,0 +1,34 @@
+const databaseHelper = require('../database/helper')
+const COLLECTION_NAME_USERS = 'users';
+
+module.exports = {
+  getUsers: async(query) => {
+    const contains = RegExp(query.term);
+
+    const options = {
+      projection: { _id: 0 },
+    }
+
+    const filter = {
+      $or: [
+        { name: contains },
+        { userName: contains },
+      ],
+    };
+
+    const collection = await databaseHelper.getCollection(COLLECTION_NAME_USERS);
+    const users = await collection
+      .find(query?.term ? filter : {}, options)
+      .skip( query.from > 0 ? ( ( query.from - 1 ) * query.size ) : 0 )
+      .limit( query.size )
+      .toArray()
+      
+    return users
+  },
+  getUserById: async(id) => {
+    const collection = await databaseHelper.getCollection(COLLECTION_NAME_USERS);
+    const users = await collection.findOne({ id })
+
+    return users
+  },
+};
