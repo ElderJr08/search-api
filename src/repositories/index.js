31c4eeecfabbaff403b4022cf4aeadbc1bd3d341
Elder Junior <elderjunior08@gmail.com>
2021-06-28 23:41:12 -0300

    refactor: Remove reverance repository

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,7 +1,5 @@
-const relevantRepository = require('./relevant-repository');
 const userRepository = require('./user-repository');
 
 module.exports = {
-  ...relevantRepository,
   ...userRepository,
 }
